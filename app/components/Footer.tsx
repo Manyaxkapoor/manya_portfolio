9dd839a7b71245a7ade28d85b2fbe3cbb1fd8353
Manya <147073441+Manyaxkapoor@users.noreply.github.com>
2026-01-02 13:15:13 +0530

    link update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -32,7 +32,7 @@ export default function Footer(): React.JSX.Element {
             </a>
 
             <a
-              href="https://drive.google.com/file/d/12mOm9r_SNaXRwXZX2vRbQEq-LvlvLZSy/view?usp=sharing"
+              href="https://drive.google.com/file/d/1RDGSTVeM7xrBWZXzBj1Vp2EBebAX6iSy/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center justify-center px-6 py-3 rounded-lg
